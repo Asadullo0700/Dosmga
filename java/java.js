1119ae426613ff0e4c94549e193b26f01844334e
Asadullo <103917411+Asadullo0700@users.noreply.github.com>
2022-04-29 10:04:57 +0500

    Add files via upload

---
+++ java/java.js
@@ -0,0 +1,35 @@
+"use strict";
+//console.log bu-console oynasiga berilgan m'alumotni chiqaradi
+console.log("Main.Java ulandi");
+//document.write bu-dokumentga HTML ga berilgan ma'lumotni yozadi
+document.write("........");
+//alert bu-berilgan ma'lumotni ekranda chiqarib beradi
+alert("Assalomu alaykum xush kelibsiz!!!")
+//tasdiqlash uchun ishlatiladi 
+confirm("Bir-nechta savol berishimizga rozimsiz?")
+
+// shu bolishi ham mumkin bu- malumot kiritish uchun
+// prompt("Biratishka bir oyoq isming nima?")
+
+console.log(prompt("Biratishka bir oyoq isming nima?"))
+
+//let bu - ozgaruvchi nomi va yaratadi
+let ass;
+//bu- shu ozgaruvchiga qiymat beradi
+ass = "Mallades choqin yaxshida "
+console.log(ass)
+document.write(ass)
+alert("G'alati masmi choqin?")
+
+
+prompt("Ha yaxshi yoshing nechida quyon")
+alert("rostanmi aldama")
+let ha;
+
+
+
+
+
+
+
+
